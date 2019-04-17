11c04f0a10c1f7fa5969072839edd08450aad857
Tanakorn <tnksp1994@gmail.com>
2019-04-17 22:39:27 +0700

    Add feature find route by max distance

--- core/findNumberOfTrips.go
+++ core/findNumberOfTrips.go
@@ -1,6 +1,6 @@
 package core
 
-func findNuberOfTrips(railWays map[string]map[string]int, sStart string, sStop string, cStop int,
+func findNuberOfTripsWithStop(railWays map[string]map[string]int, sStart string, sStop string, cStop int,
 	maxStop int, isNeedExactlyStop bool) int {
 
 	count := 0
@@ -17,9 +17,29 @@ func findNuberOfTrips(railWays map[string]map[string]int, sStart string, sStop s
 	}
 
 	cStop += +1
-	for k, _ := range railWays[sStart] {
-		c := findNuberOfTrips(railWays, k, sStop, cStop, maxStop, isNeedExactlyStop)
-		count += c
+	for city, _ := range railWays[sStart] {
+		n := findNuberOfTripsWithStop(railWays, city, sStop, cStop, maxStop, isNeedExactlyStop)
+		count += n
+	}
+
+	return count
+}
+
+func findNuberOfTripsWithDistance(railWays map[string]map[string]int, sStart string, sStop string, cDistance int,
+	maxDistance int) int {
+	count := 0
+	if cDistance >= maxDistance {
+		return 0
+	}
+
+	if sStart == sStop && cDistance > 0 {
+		count += 1
+	}
+
+	for city, _ := range railWays[sStart] {
+		d := cDistance + railWays[sStart][city]
+		n := findNuberOfTripsWithDistance(railWays, city, sStop, d, maxDistance)
+		count += n
 	}
 
 	return count
--- core/findNumberOfTrips_test.go
+++ core/findNumberOfTrips_test.go
@@ -7,15 +7,29 @@ import (
 )
 
 func Test_NumberOfTrips_SUCCESS(t *testing.T) {
-	res := findNuberOfTrips(mockRailWays, "C", "C", 0, 3, false)
-	res2 := findNuberOfTrips(mockRailWays, "A", "C", 0, 4, true)
+	res := findNuberOfTripsWithStop(mockRailWays, "C", "C", 0, 3, false)
+	res2 := findNuberOfTripsWithStop(mockRailWays, "A", "C", 0, 4, true)
 	assert.Equal(t, 2, res)
 	assert.Equal(t, 3, res2)
 }
 
 func Test_NumberOfTrips_NotFoundCase(t *testing.T) {
-	res := findNuberOfTrips(mockRailWays, "C", "Z", 0, 10, false)
-	res2 := findNuberOfTrips(mockRailWays, "A", "Y", 0, 10, true)
+	res := findNuberOfTripsWithStop(mockRailWays, "C", "Z", 0, 10, false)
+	res2 := findNuberOfTripsWithStop(mockRailWays, "A", "Y", 0, 10, true)
+	assert.Equal(t, 0, res)
+	assert.Equal(t, 0, res2)
+}
+
+func Test_findNuberOfTripsWithDistance_SUCCESS(t *testing.T) {
+	res := findNuberOfTripsWithDistance(mockRailWays, "C", "C", 0, 10)
+	res2 := findNuberOfTripsWithDistance(mockRailWays, "C", "C", 0, 30)
+	assert.Equal(t, 1, res)
+	assert.Equal(t, 7, res2)
+}
+
+func Test_findNuberOfTripsWithDistance_NotFoudRoute(t *testing.T) {
+	res := findNuberOfTripsWithDistance(mockRailWays, "C", "G", 0, 10)
+	res2 := findNuberOfTripsWithDistance(mockRailWays, "C", "C", 0, 0)
 	assert.Equal(t, 0, res)
 	assert.Equal(t, 0, res2)
 }
